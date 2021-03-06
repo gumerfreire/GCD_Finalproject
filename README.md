cf4b09e9d141b154ea0aad897031b4fadf8ece56
Gumer Freire <gumerfreire@gmail.com>
2021-03-06 23:43:39 +0100

    Initial commit of complete project. The Code Book file is not included in this commit.

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
-# GCD_Finalproject
-Getting and Cleaning Data Final Project
+# Getting and Cleaning Data Final Project
 
 Gumer Freire
 
 2021.03.06
 
-README FILE FOR GETTING AND CLEANING DATA FINAL PROJECT
+README
 --------
 
-Test text
+This project aims to process two different data sets to produce one tidy data set
+containing the informaation of the initial two (train and test data sets).
+
+The final table contains, apart from the variables, extra columns for subject and
+activity identification. The output table has labels for the columns in order to
+identify each feature. The codes for the activities are changed as well to introduce
+the names in order to be better understandable.
+
+The script is commented in each section. It comprises the following steps:
+
+1. Load data
+2. Add columns and join data from two different data sets
+3. Assign column names and change variable names for readability
+4. Summary and output
+
+The output of the script is the file "TidyData.txt" which contains the complete
+data set once processed.
---
+++ run_analysis.r
@@ -0,0 +1,83 @@
+# Getting and cleaning data project
+# Gumer Freire
+
+# Script to perform data processing
+# It is assumed that the data is in a "/dataset" subfolder of working directory
+
+# Output of the script
+# complete_table - contains the complete tidy dataset
+# summary_table - contains average and std deviation per subject and activity
+
+# Steps of the script:
+# 1. Load data
+# 2. Add columns and join data from two different data sets
+# 3. Assign column names and change variable names for readability
+# 4. Summary and output
+
+
+
+
+#Load libraries
+library(dplyr)
+
+# Load common information. Labels for features and activities
+features <- read.table("dataset/features.txt", header=FALSE)
+activity_labels <- read.table("dataset/activity_labels.txt", header=FALSE)
+
+# Load test dataset
+x_test <- read.table("dataset/test/X_test.txt", header=FALSE)
+y_test <- read.table("dataset/test/y_test.txt", header=FALSE)
+subject_test <- read.table("dataset/test/subject_test.txt", header=FALSE)
+
+# Test dataset table
+# Build a table adding the subject and activity columns to each observation
+# train_table contains columns for subject, activity and the 512 features.
+test_table <- cbind(subject_test, y_test, x_test)
+
+# Load train dataset
+x_train <- read.table("dataset/train/X_train.txt", header=FALSE)
+y_train <- read.table("dataset/train/y_train.txt", header=FALSE)
+subject_train <- read.table("dataset/train/subject_train.txt", header=FALSE)
+
+# Train dataset table
+# Build a table adding the subject and activity columns to each observation
+#train_table contains columns for subject, activity and the 512 features.
+train_table <- cbind(subject_train, y_train, x_train)
+
+# Complete table
+# Build a table the two datasets
+# complete_table contains the complete set of data and train observations.
+complete_table <- rbind(test_table, train_table)
+
+# Assign column names to the table
+# Extract feature names from loaded data and add subject and activity labels
+column_names <- c("subject","activity", as.vector(features[,2]))
+colnames(complete_table) = column_names
+
+# Assign readable activity names to the original codes in the dataset
+# First the names of the activities are changed to lowercase
+# Second, the codes in the table are changed for the activity name
+activity_labels[2] <- lapply(activity_labels[2], tolower)
+
+for (i in c(1:6)) {
+  complete_table$activity <- sub(as.character(activity_labels[i,1]), activity_labels[i,2], complete_table$activity)
+}
+
+
+# POINT 4 OF PROJECT. TIDY DATA SET
+# Write data to text file
+write.table(complete_table, "TidyData.txt", row.name=FALSE)
+
+
+# ------------------------------------------------------------------
+# POINT 5 OF PROJECT. MEAN & STD SUMMARY OF DATA SET
+
+# Remove duplicate labels in table. This point needs to be clarified with the
+# dataset. ¿Are duplicate variable names algo duplicate data or does it exist
+# any problem with variable names in the original dataset?
+complete_table <-complete_table[,unique(names(complete_table))]
+# This operation is necessary in order to group the data.
+
+# Summarize data according to point 5 of project
+complete_table %>% group_by(subject,activity) %>% summarise_all(mean, std)
+
